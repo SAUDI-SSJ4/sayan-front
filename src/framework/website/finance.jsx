8215a9728787738b210e8ce16577bd8651a2f661
SAUDI-SSJ4 <vaap82011@gmail.com>
2024-11-04 14:30:56 +0300

    حذف

--- src/framework/website/finance.jsx
+++
@@ -1,6 +0,0 @@
-import { deleteData, getData, putData, sendData } from "./production";
-
-export function useFinance() {
-  let { data, isLoading, errors } = getData("/packages");
-  return { data, isLoading, errors };
-}
